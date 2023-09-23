8c22ef26c7dabb32b8d6f457436811b8bb13ef3e
NikinikitaN <119474174+NikinikitaN@users.noreply.github.com>
2023-09-23 21:39:18 +0530

    Update interact.ts

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -1,30 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config(); // Load environment variables from .env file
-import { ethers } from "ethers";
+import { ethers, providers } from "ethers";
 const hre = require("hardhat");
-const EnergyAuditJSON = require("./../artifacts/contracts/Energy_Auditing_Certifications.sol/EnergyAudit.json");
+const EnergyAudit = require("./../artifacts/contracts/Energy_Auditing.sol/EnergyAudit.json");
+//const ContinuousAudits = require("./../artifacts/contracts/Continuous_Audits.sol/Lock.json");
 
 const contractAddress: string = process.env.CONTRACT_ADDRESS || "";
-const contractABI: any[] = EnergyAuditJSON.abi;
+const contractABI: any[] = EnergyAudit.abi;
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_NODE_URL || "");
-  const signer = await hre.ethers.getSigner();
+//   const provider = new ethers.JsonRpcProvider(process.env.RPC_NODE_URL || "");
+const provider = new providers.JsonRpcProvider(process.env.RPC_NODE_URL || "");  
+const signer = await hre.ethers.getSigner();
   const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
   try {
     // Call functions here
-    //const createproduct = await contract.certifyRenewableEnergy();
-    const createproduct = await contract.certifyRenewableEnergy({ from:"certifyRenewableEnergy" });
-  await createproduct.wait();
+    const createContract = await contract.certifyRenewableEnergy();
+  await createContract.wait();
   console.log("product created");
-  console.log("The transaction hash is:", createproduct.hash);
-  const receipt = await createproduct.wait();
+  console.log("The transaction hash is:", createContract.hash);
+  const receipt = await createContract.wait();
   console.log(
     "The transaction returned the following transaction receipt:\n",
     receipt,
   );
-    
+  
   } catch (error) {
     console.error("Error:", error);
   }

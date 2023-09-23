f24227593c25613ab4a45cfc1a963febdc7055fd
NikinikitaN <119474174+NikinikitaN@users.noreply.github.com>
2023-09-23 21:38:36 +0530

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,14 +6,14 @@ async function main() {
   console.log("ContinuousAudits deployed to:", Deployer.address);
 
   const EnergyAudit = await ethers.getContractFactory("EnergyAudit");
-  const energyAudit = await EnergyAudit.deploy("abc","srs",100,"0xdD2FD4581271e230360230F9337D5c0430Bf44C0","0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199");
+  const energyAudit = await EnergyAudit.deploy();
   console.log("EnergyAudit deployed to:", (energyAudit as any).address);
 
   const addresses = {
     EnergyAudit: energyAudit,
   };
   const fs = require("fs");
-  fs.writeFileSync("deployedAddresses.json", JSON.stringify("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199", null, 2));
+  fs.writeFileSync("deployedAddresses.json", JSON.stringify());
   console.log("Contract addresses written to deployedAddresses.json");
 }
 main()
